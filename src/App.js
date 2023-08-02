0983b51f912710610348cb9270f1a78be83e9386
Benofx <127517552+Benofx@users.noreply.github.com>
2023-08-02 10:41:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-//Le manifest est hosté ici : https://thomas01010.github.io/TBC-PBI/extension-manifest.json
+//Le manifest est hosté ici : https://static.tpionet.com/OFFICE4CONNECT/extension-manifest.json
 //Attention, en cas de message "invalid extension url" lors de la tentative d'ajout sur Connect, il est probable que cela soit dû à une erreur dans le manifest JSON.
 //Cela est à controler avec un validateur de format JSON.
 
-import logoPBI from './images/PBI.png';
+import logoPBI from './images/office.png';
 import React from "react";
 import './App.css';
 import * as Extensions from "trimble-connect-workspace-api";
@@ -41,11 +41,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainMenu: { title: "Tableaux de bord", icon: "https://images2.imgbox.com/49/1e/Nuz67Liq_o.png", command: "main_nav_menu_cliked", },
-      /*subMenuItems: [
-      { title: "Projet", icon: `https://images2.imgbox.com/49/1e/Nuz67Liq_o.png`, command: "submenu_1_clicked", },
-      { title: "Data", icon: `https://images2.imgbox.com/49/1e/Nuz67Liq_o.png`, command: "submenu_2_clicked", },
-      ],*/
+      mainMenu: { title: "Office 365", icon: "https://static.tpionet.com/OFFICE4CONNECT/office.png", command: "main_nav_menu_cliked", },
+      subMenuItems: [
+      { title: "Outlook", icon: `https://static.tpionet.com/OFFICE4CONNECT/outlook.png`, command: "submenu_1_clicked", },
+      { title: "Sharepoint", icon: `https://static.tpionet.com/OFFICE4CONNECT/sharepoint.png`, command: "submenu_2_clicked", },
+      { title: "Power BI", icon: `https://static.tpionet.com/OFFICE4CONNECT/pbi.png`, command: "submenu_2_clicked", },
+      { title: "One Drive", icon: `https://static.tpionet.com/OFFICE4CONNECT/onedrive.png`, command: "submenu_2_clicked", },
+      { title: "Word", icon: `https://static.tpionet.com/OFFICE4CONNECT/word.png`, command: "submenu_2_clicked", },
+      { title: "Excel", icon: `https://static.tpionet.com/OFFICE4CONNECT/excel.png`, command: "submenu_2_clicked", },
+      { title: "Power Point", icon: `https://static.tpionet.com/OFFICE4CONNECT/powerpoint.png`, command: "submenu_2_clicked", },
+      ],
       queryParams: "?taskId=16&navigate=true",
       editParams: false,
       title: "",
@@ -63,16 +68,36 @@ class App extends React.Component {
             case "extension.command":
               switch (args.data) {
                 case "main_nav_menu_cliked":
-                  this.setState({ title: "Tableau de bords projet" });
-                  this.setState({ link: "https://app.powerbi.com/reportEmbed?reportId=9fc9c128-2287-4ead-9d5e-f0e0f1b6a689&autoAuth=true&ctid=be0be093-a2ad-444c-93d9-5626e83beefc" });
+                  this.setState({ title: "Office 365" });
+                  this.setState({ link: "https://www.microsoft365.com" });
                   break;
                 case "submenu_1_clicked":
-                  this.setState({ title: "Tableau de bords projet" });
-                  this.setState({ link: "https://app.powerbi.com/reportEmbed?reportId=9fc9c128-2287-4ead-9d5e-f0e0f1b6a689&autoAuth=true&ctid=be0be093-a2ad-444c-93d9-5626e83beefc" });
+                  this.setState({ title: "Outlook" });
+                  this.setState({ link: "https://outlook.office.com/mail" });
                   break;
                 case "submenu_2_clicked":
-                  this.setState({ title: "Tableau de bords data"});
-                  this.setState({ link: "https://app.powerbi.com/reportEmbed?reportId=9fc9c128-2287-4ead-9d5e-f0e0f1b6a689&autoAuth=true&ctid=be0be093-a2ad-444c-93d9-5626e83beefc" });
+                  this.setState({ title: "Sharepoint"});
+                  this.setState({ link: "hhttps://bouyguesconstruction.sharepoint.com/_layouts/15/sharepoint.aspx" });
+                break;
+                case "submenu_3_clicked":
+                  this.setState({ title: "Power BI"});
+                  this.setState({ link: "https://app.powerbi.com/home?experience=power-bi" });
+                break;
+                case "submenu_4_clicked":
+                  this.setState({ title: "One Drive"});
+                  this.setState({ link: "https://bouyguesconstruction-my.sharepoint.com" });
+                break;
+                case "submenu_5_clicked":
+                  this.setState({ title: "Word"});
+                  this.setState({ link: "https://www.microsoft365.com/launch/word" });
+                break;
+                case "submenu_6_clicked":
+                  this.setState({ title: "Excel"});
+                  this.setState({ link: "https://www.microsoft365.com/launch/excel" });
+                break;
+                case "submenu_6_clicked":
+                  this.setState({ title: "Power Point"});
+                  this.setState({ link: "https://www.microsoft365.com/launch/powerpoint" });
                 break;
               }
               break;
